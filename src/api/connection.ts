701bac7db8c88495582cc16a28cd028a894d8d1a
JoArDiTo <sm.armando05@gmail.com>
2025-03-06 16:29:56 -0500

    Refactoring of API calls, improved error handling and load indicators in user interface components.

--- src/api/connection.ts
+++ src/api/connection.ts
@@ -4,13 +4,19 @@ import { test, detail_test } from '@mocks/localTest.json'
 const ROUTE = `${SERVER_URL}/api/template_test/`
 
 export async function getAllTests(){
-  return fetch(ROUTE)
-    .then((res) => res.json())
-    .catch(() => test)
+  try {
+    const res = await fetch(ROUTE)
+    return await res.json()
+  } catch {
+    return test
+  }
 }
 
 export async function getTestBy(id: string){
-  return fetch(`${ROUTE}${id}`)
-    .then((res) => res.json())
-    .catch(() => detail_test )
+  try {
+    const res = await fetch(`${ROUTE}${id}`)
+    return await res.json()
+  } catch {
+    return detail_test
+  }
 } 
